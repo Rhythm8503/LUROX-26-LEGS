51fe23ad5da98c63e18598f5a7ecf5be0862574e
Brian <bordonez1129@gmail.com>
2024-11-18 17:15:20 -0500

    ESP32PWM Class Initilization

---
+++ PWM_Init/PWM_Init.ino
@@ -0,0 +1,57 @@
+#include <ESP32Servo.h>
+#include "AccelStepper.h"
+
+#define PIN_UpperLeg 4
+#define PIN_LowerUpperLeg 5
+#define PIN_Knee 6
+#define PIN_LeftAnkle 7
+#define PIN_RightAnkle 15
+
+#define PWM_FREQ 50
+#define PWM_RES 16
+
+#define dirPin 1
+#define stepPin 2
+#define motorInterfaceType 3
+
+void setup() 
+{
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+
+  ESP32PWM Servo_1;
+  ESP32PWM Servo_2;
+  ESP32PWM Servo_3;
+  ESP32PWM Servo_4;
+  ESP32PWM Servo_5;
+
+  AccelStepper Stepper_1 = AccelStepper(motorInterfaceType, stepPin, dirPin);
+
+  // Stepper_1.setMaxSpeed(1600);
+  // Stepper_1.setAcceleration(960);  THESE 3 LINES OF CODE NEED TO BE TESTED FIRST BEFORE UNCOMMENTING
+  // Stepper_1.setCurrentPosition(0);
+
+  Servo_1.attachPin(PIN_UpperLeg, PWM_FREQ, PWM_RES);
+  Servo_2.attachPin(PIN_UpperLeg, PWM_FREQ, PWM_RES);
+  Servo_3.attachPin(PIN_UpperLeg, PWM_FREQ, PWM_RES);
+  Servo_4.attachPin(PIN_UpperLeg, PWM_FREQ, PWM_RES);
+  Servo_5.attachPin(PIN_UpperLeg, PWM_FREQ, PWM_RES);
+
+  writeDegree(Servo_1, 90);
+  writeDegree(Servo_2, 108);
+  writeDegree(Servo_3, 90);
+  writeDegree(Servo_4, 95);
+  writeDegree(Servo_5, 90);
+
+}
+void writeDegree(ESP32PWM &pwm, int degree) 
+{
+  uint16_t pwmValue = map(degree, 0, 180, 0, 65535);
+  pwm.write(pwmValue); 
+}
+
+void loop()
+{
+ 
+}
