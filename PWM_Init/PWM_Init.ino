9aa06a1129b3d2b02e1587b62e89f1718d893cdc
Brian <bordonez1129@gmail.com>
2024-11-25 16:27:54 -0500

    Modifications to Init
    Add to Leg Control System and Stepper Motor needs to be modified.

--- PWM_Init/PWM_Init.ino
+++ PWM_Init/PWM_Init.ino
@@ -10,8 +10,8 @@
 #define PWM_FREQ 50
 #define PWM_RES 16
 
-#define dirPin 1
-#define stepPin 2
+#define PIN_DIR_LEG 1
+#define PIN_STEP_LEG 2
 #define motorInterfaceType 3
 
 void setup() 
@@ -26,17 +26,17 @@ void setup()
   ESP32PWM Servo_4;
   ESP32PWM Servo_5;
 
-  AccelStepper Stepper_1 = AccelStepper(motorInterfaceType, stepPin, dirPin);
+  AccelStepper Stepper_1 = AccelStepper(motorInterfaceType, PIN_STEP_LEG, PIN_DIR_LEG);
 
   // Stepper_1.setMaxSpeed(1600);
   // Stepper_1.setAcceleration(960);  THESE 3 LINES OF CODE NEED TO BE TESTED FIRST BEFORE UNCOMMENTING
   // Stepper_1.setCurrentPosition(0);
 
   Servo_1.attachPin(PIN_UpperLeg, PWM_FREQ, PWM_RES);
-  Servo_2.attachPin(PIN_UpperLeg, PWM_FREQ, PWM_RES);
-  Servo_3.attachPin(PIN_UpperLeg, PWM_FREQ, PWM_RES);
-  Servo_4.attachPin(PIN_UpperLeg, PWM_FREQ, PWM_RES);
-  Servo_5.attachPin(PIN_UpperLeg, PWM_FREQ, PWM_RES);
+  Servo_2.attachPin(PIN_LowerUpperLeg, PWM_FREQ, PWM_RES);
+  Servo_3.attachPin(PIN_Knee, PWM_FREQ, PWM_RES);
+  Servo_4.attachPin(PIN_LeftAnkle, PWM_FREQ, PWM_RES);
+  Servo_5.attachPin(PIN_RightAnkle, PWM_FREQ, PWM_RES);
 
   writeDegree(Servo_1, 90);
   writeDegree(Servo_2, 108);
