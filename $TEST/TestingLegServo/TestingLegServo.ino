f3980315f4f68845b8ea48af063f835b4c24a173
Brian Ordonez <bordonez1129@gmail.com>
2025-04-16 13:08:44 -0400

    Testing Legs

--- $TEST/TestingLegServo/TestingLegServo.ino
+++ $TEST/TestingLegServo/TestingLegServo.ino
@@ -1,9 +1,9 @@
 #include <ESP32Servo.h>
-#include <AccelStepper.h>
+//#include <AccelStepper.h>
 
-#define dirPin 1
-#define stepPin 2
-#define motorInterfaceType 1  // 1: DRV8825 Driver
+// #define dirPin 1
+// #define stepPin 2
+// #define motorInterfaceType 1  // 1: DRV8825 Driver
 
 #define pin_Swing 41
 #define pin_Raise 20
@@ -14,10 +14,10 @@
 //const int homePosition[5] = {90, 92, 90, 87, 94};   // Brian's Homing Position
 const int homePosition[5] = {90, 90, 95, 85, 95};   // Venn's Homing Position
 ////const int homePosition[5] = {90, 124, 100, 97, 84};   // Bent Position #1
-const int homePosition[5] = {90, 44, 150, 77, 104};   // Bent Position #2
+//const int homePosition[5] = {90, 44, 150, 77, 104};   // Bent Position #2
 
-AccelStepper stepper(motorInterfaceType, stepPin, dirPin);
-long targetPosition = 0; // Default position
+// AccelStepper stepper(motorInterfaceType, stepPin, dirPin);
+// long targetPosition = 0; // Default position
 
 Servo servo_1;
 Servo servo_2;
@@ -40,28 +40,28 @@ void setup()
     delay(1000);
   }
 
-  Serial.begin(115200); // Start serial monitor
-  stepper.setMaxSpeed(200);       // steps per second
-  stepper.setAcceleration(100);   // steps per second squared
-  stepper.setCurrentPosition(0);  // Start at zero
+  // Serial.begin(115200); // Start serial monitor
+  // stepper.setMaxSpeed(200);       // steps per second
+  // stepper.setAcceleration(100);   // steps per second squared
+  // stepper.setCurrentPosition(0);  // Start at zero
 
-  Serial.println("Enter a target position (in steps):");
+  // Serial.println("Enter a target position (in steps):");
 }
 
 void loop() {
-  stepper.run();
+  // stepper.run();
 
-  // Check for serial input
-  if (Serial.available()) {
-    String input = Serial.readStringUntil('\n');  // Read input until Enter is pressed
-    input.trim();  // Remove whitespace
-    if (input.length() > 0) {
-      long newTarget = input.toInt(); // Convert input to integer
-      targetPosition = newTarget;
-      stepper.moveTo(targetPosition);
-      Serial.print("New target set to: ");
-      Serial.println(targetPosition);
-    }
-  }
+  // // Check for serial input
+  // if (Serial.available()) {
+  //   String input = Serial.readStringUntil('\n');  // Read input until Enter is pressed
+  //   input.trim();  // Remove whitespace
+  //   if (input.length() > 0) {
+  //     long newTarget = input.toInt(); // Convert input to integer
+  //     targetPosition = newTarget;
+  //     stepper.moveTo(targetPosition);
+  //     Serial.print("New target set to: ");
+  //     Serial.println(targetPosition);
+  //   }
+  // }
 
 }
